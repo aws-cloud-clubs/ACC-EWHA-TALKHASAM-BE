ec108051de9ded661eb8a57740c1ff2b56b35fa1
Seohyun Lee <applemint2003@gmail.com>
2025-05-19 13:09:08 +0900

    feat: 스프링부트 프로젝트 세팅

---
+++ artichat/src/main/java/com/talkhasam/artichat/ArtichatApplication.java
@@ -0,0 +1,13 @@
+package com.talkhasam.artichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtichatApplication.class, args);
+	}
+
+}
---
+++ artichat/src/test/java/com/talkhasam/artichat/ArtichatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkhasam.artichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtichatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
