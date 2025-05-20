43dc5dbd012cac892c814a73e1d5751e46ed21d2
Seohyun Lee <applemint2003@gmail.com>
2025-05-20 13:03:38 +0900

    fix: 자료형 오타 수정

--- artichat/src/main/java/com/talkhasam/artichat/domain/chatuser/repository/ChatUserRepository.java
+++ artichat/src/main/java/com/talkhasam/artichat/domain/chatuser/repository/ChatUserRepository.java
@@ -5,6 +5,6 @@ import com.talkhasam.artichat.domain.chatuser.entity.ChatUser;
 import java.util.Optional;
 
 public interface ChatUserRepository {
-    Optional<ChatUser> findByChatRoomIdAndNickname(String chatRoomId, String nickname);
+    Optional<ChatUser> findByChatRoomIdAndNickname(long chatRoomId, String nickname);
     ChatUser save(ChatUser chatUser);
 }
