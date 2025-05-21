507f041f4bfd94e0abcbd7c1639b73802c6f0056
Seohyun Lee <applemint2003@gmail.com>
2025-05-21 21:04:28 +0900

    fix: Message 필드 변경 (유저 nickname, isOwner 스냅샷으로 저장)

--- artichat/src/main/java/com/talkhasam/artichat/domain/message/entity/Message.java
+++ artichat/src/main/java/com/talkhasam/artichat/domain/message/entity/Message.java
@@ -1,35 +1,78 @@
 package com.talkhasam.artichat.domain.message.entity;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
 
 import java.time.Instant;
 
 @DynamoDbBean
+@Builder
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Message {
-    @Getter(onMethod_=@DynamoDbPartitionKey)
-    @Positive
-    public long chatUserId;
 
-    @Getter(onMethod_=@DynamoDbSortKey)
-    @Positive
-    private long id;
-
-    @Getter(onMethod_=@DynamoDbAttribute("createdAt"))
-    @NotNull
+    private long chatRoomId;    // Partition Key
+    private long id;            // Sort Key / 메시지 고유 ID
+    private long chatUserId;    // GSI Partition Key
+    private String nickname;
+    private boolean isOwner;
+    private String content;
     private Instant createdAt;
+    private Long ttlEpoch;
 
-    @Getter(onMethod_=@DynamoDbAttribute("ttlEpoch"))
-    public Long ttlEpoch;
+    @DynamoDbPartitionKey
+    @Positive
+    public long getChatRoomId() {
+        return chatRoomId;
+    }
+
+    @DynamoDbSortKey
+    @Positive
+    public long getId() {
+        return id;
+    }
+
+    @DynamoDbSecondaryPartitionKey(indexNames = "chatUserId-index")
+    @Positive
+    public long getChatUserId() {
+        return chatUserId;
+    }
+
+    @DynamoDbAttribute("nickname")
+    @NotBlank
+    public String getNickname() {
+        return nickname;
+    }
+
+    @DynamoDbAttribute("isOwner")
+    public boolean isOwner() {
+        return isOwner;
+    }
+
+    @DynamoDbAttribute("content")
+    @NotBlank
+    @Size(max = 10000)
+    public String getContent() {
+        return content;
+    }
+
+    @DynamoDbAttribute("createdAt")
+    @NotNull
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    @DynamoDbAttribute("ttlEpoch")
+    @NotNull
+    public Long getTtlEpoch() {
+        return ttlEpoch;
+    }
 }
