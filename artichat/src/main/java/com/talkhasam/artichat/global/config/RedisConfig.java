745b95bbf28a61aecf1da99c8c45b4b46d686cb4
Seohyun Lee <applemint2003@gmail.com>
2025-05-21 21:02:42 +0900

    feat: WebSocket 세팅, Redis Pub/Sub 설정

---
+++ artichat/src/main/java/com/talkhasam/artichat/global/config/RedisConfig.java
@@ -0,0 +1,56 @@
+package com.talkhasam.artichat.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.talkhasam.artichat.global.redis.RedisStompBridge;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            LettuceConnectionFactory connectionFactory,
+            ObjectMapper mapper
+    ) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(mapper);
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisListenerContainer(
+            LettuceConnectionFactory cf,
+            RedisStompBridge bridge
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(cf);
+        container.addMessageListener(bridge, new PatternTopic("/topic/chatroom/*"));
+        return container;
+    }
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisStompBridge redisStompBridge) {
+        return new MessageListenerAdapter(redisStompBridge, "onMessage");
+    }
+}
