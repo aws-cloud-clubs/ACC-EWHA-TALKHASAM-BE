241af1e8731cdad899be7d9e4fff6d8aeb0018d8
Seohyun Lee <applemint2003@gmail.com>
2025-05-26 16:58:43 +0900

    fix: Multipart 전송을 위한 MediaType.MULTIPART_FORM_DATA_VALUE 설정 추가

--- artichat/src/main/java/com/talkhasam/artichat/domain/chatroom/controller/ChatRoomController.java
+++ artichat/src/main/java/com/talkhasam/artichat/domain/chatroom/controller/ChatRoomController.java
@@ -6,6 +6,7 @@ import com.talkhasam.artichat.domain.chatroom.dto.ChatRoomResponseDto;
 import com.talkhasam.artichat.domain.chatroom.dto.UpdateProfileImageRequestDto;
 import com.talkhasam.artichat.domain.chatroom.service.ChatRoomService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,16 +18,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/chatrooms")
 @RequiredArgsConstructor
 public class ChatRoomController {
-
     private final ChatRoomService service;
 
     /** 생성 (201 Created + Location 헤더) */
-    @PostMapping
-    public ResponseEntity<ChatRoomPostResponseDto> createChatRoom(@RequestBody ChatRoomRequestDto requestDto) {
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ChatRoomPostResponseDto> createChatRoom(@RequestBody @Valid ChatRoomRequestDto requestDto) {
         ChatRoomPostResponseDto response = service.createChatRoom(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
-
 
     /** 채팅방 정보 조회 **/
     @GetMapping("/{chatRoomId}")
@@ -43,22 +42,4 @@ public class ChatRoomController {
         service.updateChatRoomProfileImg(chatRoomId, dto);
         return ResponseEntity.ok().build();
     }
-
-
-//    /** 단건 조회 (200 OK) */
-//    @GetMapping("/{id}")
-//    public ResponseEntity<ChatRoomResponseDto> getChatRoomById(@PathVariable Long id) {
-//        ChatRoom room = service.getChatRoom(id);
-//        return ResponseEntity.ok(ChatRoomResponseDto.from(room));
-//    }
-//
-//    /** 전체 조회 (200 OK) */
-//    @GetMapping
-//    public ResponseEntity<List<ChatRoomResponseDto>> getAllChatRooms() {
-//        List<ChatRoom> rooms = service.getAllChatRooms();
-//        List<ChatRoomResponseDto> result = rooms.stream()
-//                .map(ChatRoomResponseDto::from)
-//                .toList();
-//        return ResponseEntity.ok(result);
-//    }
 }
