94fb6684ba13d8b83a5b8a74c7762f7ed3dec9cf
Seohyun Lee <applemint2003@gmail.com>
2025-05-26 14:18:20 +0900

    hotfix: JacksonConfig 작성

---
+++ artichat/src/main/java/com/talkhasam/artichat/global/config/JacksonConfig.java
@@ -0,0 +1,52 @@
+package com.talkhasam.artichat.global.config;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> {
+            // ▶ Serializer: Long → String
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+            builder.serializerByType(long.class, ToStringSerializer.instance);
+
+            // ▶ Deserializer: Numeric String → Long
+            SimpleModule module = new SimpleModule();
+            module.addDeserializer(Long.class, new StdDeserializer<Long>(Long.class) {
+                @Override
+                public Long deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+                    String text = p.getText();
+                    if (StringUtils.hasText(text) && text.matches("\\d+")) {
+                        return Long.parseLong(text);
+                    }
+                    return null;
+                }
+            });
+            module.addDeserializer(long.class, new StdDeserializer<Long>(Long.TYPE) {
+                @Override
+                public Long deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+                    String text = p.getText();
+                    if (StringUtils.hasText(text) && text.matches("\\d+")) {
+                        return Long.parseLong(text);
+                    }
+                    return 0L;
+                }
+            });
+
+            builder.modules(module);
+        };
+    }
+}
